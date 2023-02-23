ab4dce1d407ce34c27e1f288291ea8269efd824a
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-02-22 18:17:33 -0600

    Toast pelicula

--- Movil/app/src/main/java/fes/aragon/movil/peliculas/Peliculas.kt
+++ Movil/app/src/main/java/fes/aragon/movil/peliculas/Peliculas.kt
@@ -2,6 +2,8 @@ package fes.aragon.movil.peliculas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import fes.aragon.movil.R
 
 class Peliculas : AppCompatActivity() {
@@ -10,6 +12,24 @@ class Peliculas : AppCompatActivity() {
         setContentView(R.layout.activity_peliculas)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
+
+        val buttonPlay: FloatingActionButton = findViewById(R.id.floatingButtonPlay)
+        val buttonComprar: FloatingActionButton = findViewById(R.id.floatingButtonComprar)
+        val buttonDetalle: FloatingActionButton = findViewById(R.id.floatingButtonDetalle)
+        val buttonSalir: FloatingActionButton = findViewById(R.id.floatingButtonSalir)
+
+        buttonPlay.setOnClickListener{
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
+        }
+        buttonComprar.setOnClickListener {
+            Toast.makeText(this, "Comprar pelicula", Toast.LENGTH_SHORT).show()
+        }
+        buttonDetalle.setOnClickListener {
+            Toast.makeText(this, "Detalles", Toast.LENGTH_SHORT).show()
+        }
+        buttonSalir.setOnClickListener {
+            Toast.makeText(this, "Salir", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
