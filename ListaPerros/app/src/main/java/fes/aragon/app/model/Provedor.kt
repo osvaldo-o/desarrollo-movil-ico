cf3d1ef231fe28213016db2c881d4fa345668cd6
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-03-27 19:53:07 -0600

    Examen para calificacion max 8

---
+++ ListaPerros/app/src/main/java/fes/aragon/app/model/Provedor.kt
@@ -0,0 +1,14 @@
+package fes.aragon.app.model
+
+class Provedor {
+    companion object {
+        val listTop = mutableListOf<DataModel>(
+            DataModel("Salchicha","https://cdn.quecuriosidades.com/wp-content/uploads/2019/01/hqdefault-600x469.jpg"),
+            DataModel("Alaska","https://i.pinimg.com/236x/09/5e/85/095e85c29be70d8a36b37997d02e1d9d--malamute-dog-alaskan-malamute.jpg"),
+            DataModel("Bichón Maltés","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaGK0OO7hciHRxIgYp1yT_n4kAOyp6Uaiehg&usqp=https://www.shutterstock.com/image-photo/maltese-dog-sits-on-blanket-260nw-2016069392.jpg"),
+            DataModel("Labrador","https://www.webconsultas.com/sites/default/files/styles/wc_adaptive_image__small/public/temas/caracteristicas-perro-labrador.jpg"),
+            DataModel("Golden","https://cdn.wamiz.fr/article/images/golden-doodle-cachorro.jpg")
+        )
+        val listBottom: ArrayList<DataModel> = ArrayList()
+    }
+}
