1eb073ac18d8e758c2741c9b35c79bf4af66aeba
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-04-17 11:07:19 -0600

    PreExamen

--- AppExamenHTTP/app/src/main/java/fes/aragon/appexamenhttp/RecyclerViewAdapter.kt
+++ AppExamenHTTP/app/src/main/java/fes/aragon/appexamenhttp/RecyclerViewAdapter.kt
@@ -1,27 +1,32 @@
 package fes.aragon.appexamenhttp
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import fes.aragon.appexamenhttp.databinding.ActivityMainBinding
 import fes.aragon.appexamenhttp.databinding.ItemBinding
 
 class RecyclerViewAdapter (val list: List<Message>) : RecyclerView.Adapter<BaseViewHolder<*>>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        TODO("Not yet implemented")
+        val itemBinding = ItemBinding.inflate(LayoutInflater.from(parent.context))
+        val holder = ViewHolder(itemBinding,parent.context)
+        return holder
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        TODO("Not yet implemented")
+        when (holder) {
+            is ViewHolder -> holder.bind(list[position])
+        }
     }
 
     private inner class ViewHolder(val binding: ItemBinding,val context: Context) : BaseViewHolder<Message>(binding.root) {
         override fun bind(item: Message) {
-
+            Picasso.get().load(item.image).into(binding.image)
+            binding.name.text = item.name
         }
     }
 }
--- AppExamenHTTP/app/src/main/java/fes/aragon/appexamenhttp/ServiceApi.kt
+++ AppExamenHTTP/app/src/main/java/fes/aragon/appexamenhttp/ServiceApi.kt
@@ -1,5 +1,6 @@
 package fes.aragon.appexamenhttp
 
+import com.google.gson.GsonBuilder
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,8 +13,8 @@ interface ServiceApi {
 object RetrofitCliente {
     val webService by lazy {
         Retrofit.Builder()
-            .baseUrl("https://private-025ab1-osvaldo4.apiary-mock.com/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://private-025ab1-osvaldo4.apiary-mock.com")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .build().create(ServiceApi::class.java)
     }
 }
