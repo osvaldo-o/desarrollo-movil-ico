7b53ce957a6a9f84e6fc6b432bdafad88db56df3
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-04-20 08:51:02 -0600

    Bug Internet

--- AppExamenHTTP/app/src/main/java/fes/aragon/appexamenhttp/view/MainActivity.kt
+++ AppExamenHTTP/app/src/main/java/fes/aragon/appexamenhttp/view/MainActivity.kt
@@ -1,8 +1,14 @@
 package fes.aragon.appexamenhttp.view
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import fes.aragon.appexamenhttp.view.adapters.RecyclerViewAdapter
 import fes.aragon.appexamenhttp.databinding.ActivityMainBinding
 import fes.aragon.appexamenhttp.model.Result
@@ -19,17 +25,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        adapter = RecyclerViewAdapter(users)
-        binding.recyclerView.adapter = adapter
+        initRecyclerView()
+
         dataViewModel.user.observe(this,{
             users.add(it)
             binding.buttonAdd.isClickable = true
             adapter.notifyDataSetChanged()
         })
+
         binding.buttonAdd.setOnClickListener {
-            dataViewModel.getUser(users.size)
-            binding.buttonAdd.isClickable = false
+            if (isOnline()){
+                dataViewModel.getUser(users.size)
+                binding.buttonAdd.isClickable = false
+            }else{
+                Toast.makeText(this,"No hay conexion a internet",Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun initRecyclerView() {
+        adapter = RecyclerViewAdapter(users)
+        binding.recyclerView.adapter = adapter
+    }
+
+    fun isOnline(): Boolean {
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = connMgr.activeNetworkInfo
+        return networkInfo?.isConnected == true
     }
 
 }
