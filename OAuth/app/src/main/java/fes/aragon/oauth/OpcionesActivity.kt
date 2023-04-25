64b0da04db3c8656ec4f8e08a4a4933653d5a421
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-04-24 21:40:13 -0600

    App Auth correo,google

---
+++ OAuth/app/src/main/java/fes/aragon/oauth/OpcionesActivity.kt
@@ -0,0 +1,11 @@
+package fes.aragon.oauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpcionesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opciones)
+    }
+}
